139c045d18fc82099d77adeb135a407d90838514
Megan <mhmeluskey@gmail.com>
2018-11-12 19:25:07 -0500

    alert for repeated letter

--- assets/app.js
+++ assets/app.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     console.log("ready!");
 
     possibleGuessArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-    userGuessArray = []; 
+    userGuessArray = [];
 
 
     var randomWords = [
@@ -23,13 +23,13 @@ $(document).ready(function () {
         $("#word").html("New Word is: " + amount + " letters long.")
     })
 
-    
-    var str = word; 
+
+    var str = word;
     var array = str.split("");
     console.log(array);
-    
 
-   
+
+
 
 
     for (var i = 0; i < array.length; i++) {
@@ -38,32 +38,40 @@ $(document).ready(function () {
         }).appendTo("#word-guessed");
 
 
-}
+    }
     //event listener 
     document.onkeyup = function (event) {
         var userGuess = event.key;
         console.log(userGuess);
-    
-        if (possibleGuessArray.includes(userGuess))
 
-      {
-            if (word.includes(userGuess)) {
-                console.log(userGuess + " is correct");
-                var correctLetter = userGuess; 
-                console.log(correctLetter);   
-            } else {
-                $("#guesses").append(userGuess + "-");
-                console.log("You guessed the wrong letter")
-            }
+
+        if (userGuessArray.includes(userGuess)) {
+
+            confirm("You have already guessed letter " + userGuess);
 
         } else {
-            confirm(userGuess + " is not a valid guess, please enter a letter!")
-        }
+            if (possibleGuessArray.includes(userGuess)) {
+                if (word.includes(userGuess)) {
+                    console.log(userGuess + " is correct");
+                    var correctLetter = userGuess;
+                    console.log(correctLetter);
+                } else {
+                    $("#guesses").append(userGuess + "-");
+                    console.log("You guessed the wrong letter");
+                    userGuessArray.push(userGuess);
+                    console.log(userGuessArray);
+                }
 
 
+            } else {
+                confirm(userGuess + "is not a valid guess. Please enter a letter!")
+            }
 
-    }//on key up
+        } //end of else for userGuessArray.includes(userGuess)
+
+    } //document on key up
+
+}); //document.ready
 
 
-
-}); //doc.ready function
+//next step will be to assign ids for each div that cooresponds to the letter that will go there
