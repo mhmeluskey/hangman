e16b22573c35635fb01e63ea96065160b54cea13
Megan <mhmeluskey@gmail.com>
2018-11-13 21:00:43 -0500

    added modal

--- assets/app.js
+++ assets/app.js
@@ -1,9 +1,17 @@
 $(document).ready(function () {
+$("#new-game").click()
+function start() {
+  
+
+
     console.log("ready!");
 
     possibleGuessArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
     userGuessArray = [];
     correctGuessArray = [];
+    htmlGuesses = 10; 
+    losses = 0;
+    wins = 0;
 
 
 
@@ -13,7 +21,27 @@ $(document).ready(function () {
         "america",
         "bootcamp",
         "javascript",
-        "philadelphia"
+        "philadelphia",
+        "president",
+        "accountant",
+        "web",
+        "designer",
+        "digital",
+        "html",
+        "react",
+        "mysql",
+        "jquery",
+        "winter",
+        "spring",
+        "computer",
+        "mac",
+        "windows",
+        "mouse",
+        "display",
+        "adobe",
+        "photoshop",
+        "finalcut",
+        
     ]
     var word = randomWords[Math.floor(Math.random() * randomWords.length)]; {
         console.log(word);
@@ -33,15 +61,12 @@ $(document).ready(function () {
 
     for (var i = 0; i < lettersOfWordArray.length; i++) {
         jQuery('<div/>', {
-            class: "blank-box",
-            value: i,
-            id: lettersOfWordArray[i]
+            class: lettersOfWordArray[i],
+            id: "blank-box" //not right to have multiple divs with same id, unsure about how to fix this but app is still working like i want it to
         }).appendTo("#word-guessed");
     }
 
-    var ids = [];
-    $(".blank-box").each(function () { ids.push(this.id); });
-    console.log("ids: " + ids);
+
 
     //if the letter guessed equals the id of the div, append the user guess to that div
 
@@ -60,19 +85,27 @@ $(document).ready(function () {
 
             if (possibleGuessArray.includes(userGuess)) {
                 if (word.includes(userGuess)) {
-
-                   
                     console.log(userGuess + " is correct");
                     correctGuessArray.push(userGuess);
-                    $("#" + userGuess).append(userGuess);
-                    
-                
+                    $("." + userGuess).append(userGuess);
 
+                    
+    
                 } else {
                     $("#guesses").append(userGuess + "-");
                     console.log("You guessed the wrong letter");
                     userGuessArray.push(userGuess);
                     console.log(userGuessArray);
+                    $("#losses").html("Losses: " + ++losses);
+                    $("#chances").html("Guesses: " + --htmlGuesses);
+
+                    if (userGuessArray.length >= 10) {
+                        console.log("Game Over")
+                        displayModal();
+                      
+
+
+                    }
                 }
 
 
@@ -86,6 +119,38 @@ $(document).ready(function () {
         }//end of else for userGuessArray.includes(userGuess)
 
     } //document on key up
+
+}
+
+
+    // Get the modal
+var modal = document.getElementById('myModal');
+
+// // Get the button that opens the modal
+// var btn = document.getElementById("myBtn");
+
+// // Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+function displayModal() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+start(); 
 
 }); //document.ready
 
